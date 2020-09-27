35919b3ad5820a15f69ab2d5ae32636fa85c672b
Daniel <dalor90@hotmail.com>
2020-09-27 16:10:33 -0300

    Add files via upload

---
+++ Powershell/base64.ps1
@@ -0,0 +1,26 @@
+﻿
+Function Base64(){
+Param(
+    [Parameter(ValueFromPipeline=$true)]
+    $Decode,
+    [Parameter(ValueFromPipeline=$true)]
+    $Encode
+)
+
+    if(($Decode -ne $null -or $Decode -ne "") -and ($Encode -eq $null -or $Encode -eq "")){
+        try{
+            $result = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($Decode))
+        }catch{
+            
+        }
+    }elseif(($Decode -eq $null -or $Decode -eq "") -and ($Encode -ne $null -or $Encode -ne "")){
+            try{
+                $result = [Convert]::ToBase64String([Text.Encoding]::UTF8.GetBytes($Encode))
+            }catch{
+
+            }
+    }
+
+    return $result
+
+}
